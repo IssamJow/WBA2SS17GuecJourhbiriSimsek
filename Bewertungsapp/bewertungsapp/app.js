836fe1080870a2e1f17a87a38661fc4d2e515ac0
Issam Jourhbiri <issamjow@hotmail.fr>
2017-06-30 11:44:57 +0000

    User Express Framework implementiert

--- Bewertungsapp/bewertungsapp/app.js
+++ Bewertungsapp/bewertungsapp/app.js
@@ -1,76 +1,45 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const fs = require ("fs");
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+global.data = require("./data");
 
 var app = express();
-// GET-Methoden
-app.get('/bewertungen', function (req, res){
-    res.send('GET Bewertungen zum Professor');
+
+const settings = {
+    port: 3000,
+    datafile: "./testdata.json"    
+};
+app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.send(err.status + ' ' + err.messages);
 });
-app.get('/professor', function (req, res){
-    res.send('GET die vorhandene Professoren');
+app.use(function (req, res, next) {
+    console.log('Time: %d ' + ' Request-Pfad: ' + req.path, Date.now());
+    next();
+}); 
+
+//Routing
+
+//binden des Pfades an eine Konstante
+const user = require('./user');
+// mounten des Routers für "user" an die main App
+app.use("/user", user);
+
+// Get Request auf pfad "/"
+app.get("/", function(req,res) {
+    res.send("Get Hello Darling!");
 });
-app.get('/wiss_mitarbeiter', function (req, res){
-    res.send('GET die vorhandene Mitarbeitern');
+app.post("/", function (req,res) {
+    res.send("POST")
 });
-app.get('/tutor', function (req, res){
-    res.send('GET die vorhandene Tutoren');
+app.put("/user", function (req, res){
+        res.send("PUT /user")
+        })
+app.delete('/user', function (req, res) {
+    res.send("DELETE /user")
+})
+
+app.listen(settings.port, function(){
+    console.log("Express App läuft auf Port " + settings.port);
 });
-
-
-// POST-Methoden
-app.post('/', function (req, res){
-    res.send('POST Bewertungen zum Professor')
-});
-app.post('/professor', function (req, res){
-    res.send('Neuen Professor anlegen')
-});
-app.post('/wiss_mitarbeiter', function (req, res){
-    res.send('Neuen Mitarbeiter anlegen')
-});
-app.post('/tutor', function (req, res){
-    res.send('Neuen Tutor anlegen')
-});
-
-
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
