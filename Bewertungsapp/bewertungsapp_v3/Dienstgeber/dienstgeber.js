7b89c4386267b50de44b5bd53961a6cc29e7d9bd
Issam Jourhbiri <issamjow@hotmail.fr>
2017-07-16 20:07:36 +0200

    get mit ID Methode

--- Bewertungsapp/bewertungsapp_v3/Dienstgeber/dienstgeber.js
+++ Bewertungsapp/bewertungsapp_v3/Dienstgeber/dienstgeber.js
@@ -10,6 +10,7 @@ var settings = {
     port: 3000,
     datafile: "./Json/testdata.json"
 };
+global.data = require("./data");
 
 const users = require('./user/index');
 const personen = require('./personen/index');
@@ -31,16 +32,6 @@ app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
-});
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
 });
 
 
--- Bewertungsapp/bewertungsapp_v3/Dienstgeber/user/index.js
+++ Bewertungsapp/bewertungsapp_v3/Dienstgeber/user/index.js
@@ -6,10 +6,15 @@ var bodyParser = require('body-parser');
 const ressourceName = "users";
 
 router.get('/', function (req, res){
-    res.send("Zeigt alle User");
+    res.status(200).send(data.users);
 });
 
-
+router.get("/:id", function (req, res){
+    var id = parseInt(req.params.id);
+    
+    if (isNaN(id)) { res.status(400).json(data.errors.badParameters)}
+    //TODO sind noch hier dabei
+})
 router.post('/', bodyParser.json(), function (req, res) {
     console.log(req.body);
     res.status(200).json( {uri: req.protocol+"://" + req.headers.host + "/"+ressourceName+"/"+req.body.id});
