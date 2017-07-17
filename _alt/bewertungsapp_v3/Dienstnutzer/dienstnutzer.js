db2b7b0d9b3f50dfee1a6bd7421755ab302952d4
Issam Jourhbiri <issamjow@hotmail.fr>
2017-07-17 21:51:39 +0200

    Dienstgeber und Dienstnutzer erfolgreich zu ende implementiert

--- _alt/bewertungsapp_v3/Dienstnutzer/dienstnutzer.js
+++ _alt/bewertungsapp_v3/Dienstnutzer/dienstnutzer.js
@@ -1,93 +1,33 @@
-var express = require('express'),
-    http = require('http'),
-    request = require('request');
+var express = require('express');
+var async = require('async');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+
+
+
 var app = express();
-
-var dHost = 'http://localhost';
-var dPort = 3000;
-var dURL = dHost + ':' + dPort;
-
-
-// Get-Requests
-
-app.get('users', function (req, res) {
-    var url = dURL + '/users';
-
-    //TODO : Get-Request implementieren
-    request(url, function (err, response, body) {
-        body = JSON.parse(body);
-        res.json(body);
-
-    });
+var settings = {
+    port: 3001
+};
 
 
+const ratings = require('./rating/index');
+app.use("/rating", ratings);
+
+
+
+app.listen(settings.port, function(){
+    console.log("Dienstnutzer ist nun auf Port " +settings.port+ " verfügbar.");
 });
 
-// Get-Requests
+app.get('/', function(req, res){
+    res.send('Hello Nutzer');
+    
+}); 
 
-app.get('/users/:userID', function (req, res) {
-
-    var userID = req.params.userID;
-    var url = dHost + ':' + dPort + '/users/' + userID;
-
-    //TODO Get-Requests implementieren
-    request.get(url, function (err, response, body) {
-        body = JSON.parse(body);
-        res.json(body);
-
-
-    });
-
-
-});
-
-// POST
-
-app.post('/users', function (req, res) {
-    var userData = {
-        "name": "",
-        "fakultaet": 0,
-        "studiengaenge": "",
-        "email": "",
-        "passwort": "",
-        "id": 0
-
-    };
-
-    var url = dURL + '/users';
-
-    //TODO Post-Request implementieren
-    var options = {
-        uri: url,
-        method: 'POST',
-        headers: {
-            'Content-Type: application/json'
-        },
-        json: userData
-    };
-
-    request(options, function (err, response, body) {
-        res.json(body);
-    });
-
-});
-
-// Delete-Request
-
-app.delete('/users/:id', function (req, res) {
-
-    var id = req.params.id;
-    var url = dURL + '/users/' + id;
-
-    //TODO Delete-Request implementieren
-    request.delete(url, function (err, response, body) {
-        res.json(body);
-
-    });
-
-});
-
-app.listen(8080, function () {
-    console.log("Dienstnutzer ist nur auf Port 8080 verfuegbar");
-
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
---
+++ _alt/bewertungsapp_v3/Dienstnutzer/rating/index.js
@@ -0,0 +1,145 @@
+/*
+index für Personen
+*/
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+const bodyParser = require('body-parser');
+
+
+
+var _jsonFile = __dirname + '/Json/ratings.json';
+
+
+
+
+function getRatingFromData() {
+    //fs readfile
+    var content = fs.readFileSync(_jsonFile).toString();
+  
+    var tempContent = content.substr(0, content.length -1);
+    var contentObjekt = "[" + tempContent + "]";
+    //console.log(tempContent);
+    return JSON.parse(contentObjekt);
+}
+
+
+
+function checkIfExist(newRating, ratingList) {
+    var temp = false;
+
+    for (var i = 0; i < ratingList.length; i++) {
+        if (ratingList[i].id == newRating.id) {
+            temp = true;
+            break;
+        }
+
+    }
+    return temp;
+}
+
+function updateFile(ratingList) {
+    fs.truncate(_jsonFile, 0, function (err) {
+        for (var i = 0; i < ratingList.length; i++) {
+            var line = JSON.stringify(ratingList[i]) + ",";
+            fs.appendFile(_jsonFile, line, function (err) {});
+        }
+
+    });
+}
+
+
+
+function checkId(ratingId, ratingList) {
+    var temp = false;
+
+    for (var i = 0; i < ratingList.length; i++) {
+        if (ratingList[i].id == ratingId) {
+            temp = true;
+            break;
+        }
+
+    }
+    return temp;
+}
+router.get('bestof', function(req,res){
+    var ratingList = bestOf();
+    res.send(ratingList);
+})
+router.get('/', function (req, res) {
+    var ratingList = getRatingFromData();
+    res.send(ratingList);
+});
+router.get('/:id', function(req, res) {
+    var ratingId = req.params.id;
+    var ratingList = getRatingFromData();
+    var check = checkId(ratingId, ratingList);
+    
+    if( check == false){
+      res.send("Rating mit der id " + ratingId + " ist nicht vorhanden");
+    }
+    else 
+        {
+            for(var i = 0; i< ratingList.length;i++){
+                if(ratingList[i].id == ratingId)
+                    {
+                        res.send(ratingList[i]);
+                    }
+                
+            }
+        }
+});
+router.post('/', bodyParser.json(), function (req, res) {
+    var newRating = req.body;
+    var ratingList = getRatingFromData();
+    var check = checkIfExist(newRating, ratingList);
+    if (check == true) {
+        res.send("Rating schon vorhanden");
+    } else {
+        var line = JSON.stringify(newRating) + ",";
+        fs.appendFile(_jsonFile, line, function (err) {
+            res.status(201).send("Rating erfolgreich erstellt.");
+        });
+    }
+
+});
+router.put('/:id', bodyParser.json(), function (req, res) {
+    var rating = req.body;
+    rating.id = req.params.id;
+    var ratingList = getRatingFromData();
+    var check = checkIfExist(rating, ratingList);
+    if (check == false) {
+        res.send("Rating mit der id " + rating.id + " ist nicht vorhanden");
+    } else {
+        for (var i = 0; i < ratingList.length; i++) {
+            if (ratingList[i].id == rating.id) {
+                ratingList[i] = rating;
+                updateFile(ratingList);
+                res.send("Rating mit der ID " + ratingList[i].id + " wurde ersetzt");
+                break;
+            }
+
+        }
+    }
+
+});
+router.delete('/:id', function (req, res) {
+    var ratingId = req.params.id;
+    var ratingList = getRatingFromData();
+    var check = checkId(ratingId, ratingList);
+    if (check == false) {
+        res.send("Rating mit der id " + ratingId + " ist nicht vorhanden");
+    } else {
+        for (var i = 0; i < ratingList.length; i++) {
+            if (ratingList[i].id == ratingId) {
+                ratingList.splice(i, 1);
+                updateFile(ratingList);
+                res.send("Rating mit der ID " + ratingId + " wurde erfolgreich gelöscht");
+                break;
+            }
+        }
+    }
+});
+
+
+module.exports = router;
